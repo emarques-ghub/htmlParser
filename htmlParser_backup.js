51eb659283c13a9623b435ce50f825029bf933d6
Eduardo Marques <emarques@outlook.com>
2021-07-03 21:21:57 -0300

    v1

--- htmlParser_backup.js
+++ htmlParser_backup.js
@@ -37,80 +37,78 @@ document.getElementById('fileInput').addEventListener('change', function selecte
     var rg_nbsp = /&nbsp;/
     var transacao = {data: "", memo: "", valor: "", parcela: ""}
     var resultado = ""
-    var resumo = ""
     var soma = 0
 
     do {
       retorno = extraiProximaLinha(retorno[0]+1, reader.result);
-
-      if (/Compras parceladas - pr(.*)imas faturas/.test(retorno[1]) ) {
-        criarElemento("Total Fatura:" + resumo + "Total Lido:" + soma.toFixed(2))
-        criarElemento(" ")
-        criarElemento("Compras parceladas - proximas faturas")
-        resumo = 0
-        soma = 0 
-      }
-
-      if (/Total desta fatura/.test(retorno[1])) {
-        resumo = transacao.valor
-      }      
+      //linha = retorno[1].replace("<NOBR>", "").replace("</NOBR>", "")//.replace("&nbsp;", "")
 
       if (/(<TR>)/.test(retorno[1])) {
-        transacao = {data: "", memo: "", valor: "", parcela: ""}         
+        transacao = {data: "", memo: "", valor: "", parcela: ""}         //}
         while (true) {
             retorno = extraiProximaLinha(retorno[0]+1, reader.result);
             retorno[1] = retorno[1].replace("<NOBR>", "").replace("</NOBR>", "").replace("<SPAN>", "").replace("</SPAN>", "")
 
-            if (/Compras parceladas - pr(.*)imas faturas/.test(retorno[1]) ) {
-              criarElemento("Total Fatura:" + resumo + "Total Lido:" + soma.toFixed(2))
-              criarElemento(" ")
-              criarElemento("Compras parceladas - proximas faturas")
-              resumo = 0
-              soma = 0 
-            }
-
             if (/(<\/TR>)/.test(retorno[1]) || retorno[0] >= reader.result.length)
                 break;
             else {
-                if (/Total desta fatura/.test(retorno[1])) {
-                  retorno = extraiProximaLinha(retorno[0]+1, reader.result);
-                  linha = /(">)([^<>].*)(<\/P>)/.exec(retorno[1])[0].replace('">', "").replace("</P>", "")
-                  resumo = linha
-                  break
-                }
-  
+
                 if ( /<TD(.*)<\/TD>/.test(retorno[1]) && /(">)([^<>].*)(<\/P>)/.test(retorno[1])) {
                     linha = /(">)([^<>].*)(<\/P>)/.exec(retorno[1])[0].replace('">', "").replace("</P>", "")
 
-                    if (transacao.data == "") {
-                        if(rg_data.test(linha)) 
-                            transacao.data = linha
-                        if (rg_data_memo.test(linha)) {
-                            transacao.data = linha.substr(0, 5)
-                            transacao.memo = linha.substr(6)
+                    if (rg_data.test(linha))  {
+                        if (transacao.data == "") 
+                          transacao.data = linha
+                        else {
+                          transacao.memo = transacao.memo + linha
+                          if (/([0-3][0-9]\/[0-1][0-9])/.test(transacao.memo))
+                            transacao.parcela = /([0-3][0-9]\/[0-1][0-9])/.exec(transacao.memo)[0]
                         }
                     }
-                    else {
-                      if (rg_nbsp.test(linha)) { }
-                      else if (rg_valor.test(linha) && transacao.valor == "") 
-                            transacao.valor = linha
-                      else if (transacao.data != "" && transacao.valor == "") { 
-                        transacao.memo = transacao.memo + linha
+                    else if (rg_valor.test(linha)) {
+                        transacao.valor = linha 
+                    }
+                    else if (rg_data_memo.test(linha)) {
+                        transacao.data = linha.substr(0, 5)
+                        transacao.memo = linha.substr(6)
                         if (/([0-3][0-9]\/[0-1][0-9])/.test(transacao.memo))
                           transacao.parcela = /([0-3][0-9]\/[0-1][0-9])/.exec(transacao.memo)[0]
-                      }
-                    } 
-                } 
-                
-            } 
-        } //loop dentro do <TR> </TR>
+                    }
+                    else if (rg_nbsp.test(linha)) {
+                    }
+                    else { 
+                        transacao.memo = linha
+                        if (/([0-3][0-9]\/[0-1][0-9])/.test(transacao.memo))
+                          transacao.parcela = /([0-3][0-9]\/[0-1][0-9])/.exec(transacao.memo)[0]
+                    }
+                  }
+            }
+        }
       
+        if (/Total desta fatura/.test(transacao.memo)) {
+          resumo = transacao.valor
+        }
+
+        /*
+        if (/Compras parceladas - pr(.*)imas faturas/.test(transacao.memo)) {
+          criarElemento("Total Fatura:" + resumo + "Total Lido:" + soma)
+          criarElemento(transacao.data + " | " + transacao.memo + " | " + transacao.valor + " | " + transacao.parcela)
+          soma = 0 
+        }*/
+
         if (transacao.data != "" && transacao.memo != "" && transacao.valor != "") {
             soma = soma + parseFloat(transacao.valor.replace("- ", "-").replace("." , "").replace("," , "."))
             criarElemento(transacao.data + " | " + transacao.memo + " | " + transacao.valor + " | " + transacao.parcela + " | " + soma.toFixed(2)) 
           }
 
-      } // FIM DO TR
+      }
+
+      if (/Compras parceladas - pr(.*)imas faturas/.test(retorno[1])) {
+        criarElemento("Total Fatura:" + resumo + "Total Lido:" + soma)
+        criarElemento("\nCompras parceladas - proximas faturas")
+        resumo = 0
+        soma = 0 
+      }
 
     } while (retorno[0] < reader.result.length);  
     criarElemento("Total Fatura:" + resumo + "Total Lido:" + soma)
